59c928c357cd8daf7f650cecb8d34022e555dd64
David Gustafson <git@radicalskeptic.net>
2014-05-20 13:39:54 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Todo::Application.routes.draw do
-  namespace :Api do
-    resources :lists
+  namespace :api do
+
+    resources :users do
+      resources :lists, except: [:index]
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create, :new]
+    end
+
+    resources :items, only: [:destroy]
+
   end
-  #namespace :Api do
-
-    #resources :users do
-      #resources :lists, except: [:index]
-    #end
-
-    #resources :lists, only: [] do
-      #resources :items, only: [:create, :new]
-    #end
-
-    #resources :items, only: [:destroy]
-
-  #end
 
   root to: 'users#new'
 end
