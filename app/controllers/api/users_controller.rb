f9597a30ea26a4312df26e45b7ee5de145299374
Jonathan Linowes <linojon@gmail.com>
2014-05-24 12:43:02 -0400

    can be a local variable instead of instance var

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,16 @@
 class Api::UsersController < ApiController
 
   def index
-    @users = User.all
-    render json: @users, each_serializer: UsersSerializer
+    users = User.all
+    render json: users, each_serializer: UsersSerializer
   end
 
   def create
-    @user = User.new(new_user_params)
-    if @user.save
-      render json: @user, root: false, status: :created # 201
+    user = User.new(new_user_params)
+    if user.save
+      render json: user, root: false, status: :created # 201
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity # 422
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity # 422
     end
   end
 
