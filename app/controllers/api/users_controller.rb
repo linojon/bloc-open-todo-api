d9814e19b89a9f3481b6632a66be9e2879f103f9
Jonathan Linowes <linojon@gmail.com>
2014-05-24 13:27:05 -0400

    refactor users_controller with stubbed permissions check

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,13 @@
 class Api::UsersController < ApiController
 
   def index
+    return if permission_denied?
     users = User.all
     render json: users, each_serializer: UsersSerializer
   end
 
   def create
+    return if permission_denied?
     user = User.new(new_user_params)
     if user.save
       render json: user, root: false, status: :created # 201
@@ -18,6 +20,14 @@ class Api::UsersController < ApiController
 
   def new_user_params
     params.permit(:username, :password)
+  end
+
+  def permission_denied?
+    permission_denied_error unless conditions_met
+  end
+
+  def conditions_met
+    true
   end
 
 end
