adb279122d4a588a8a07f18aae7bbf196653a230
Jonathan Linowes <linojon@gmail.com>
2014-05-27 18:20:34 -0400

    change conf file to yaml format

--- spec/cli/todo_spec.rb
+++ spec/cli/todo_spec.rb
@@ -5,26 +5,19 @@ describe "todo", type: :feature, js: true do
     start_api_server
   end
 
-  it "works" do
-    #puts RestClient.get "http://127.0.0.1:3003/"
-    json = RestClient.get api_url('users'), accept: :json
-    data = JSON.parse json
-    expect(data['users']).to include( { 'id' => 1, 'username' => 'testuser' } )
-  end
-
   it "login" do
-    `bin/todo login testuser secret 127.0.0.1:3003 -f test.auth`
-    expect(File.read("test.auth").strip).to eql "testuser;secret;127.0.0.1:3003"
+    `bin/todo login testuser secret 127.0.0.1:3003 -c todo.test`
+    expect(YAML::load_file("todo.test")).to eql( username: 'testuser', password: 'secret', host: '127.0.0.1:3003' )
   end
 
   it "whoami" do
-    result = `bin/todo whoami -f test.auth`
+    result = `bin/todo whoami -c todo.test`
     expect(result.strip).to eql "1: testuser"
   end
 
   it "users" do
     User.create( username: "user2", password: "password" )
-    result = `bin/todo users -f test.auth`
+    result = `bin/todo users -c todo.test`
     expect(result).to eql( 
 %Q{1: testuser
 2: user2
@@ -32,8 +25,8 @@ describe "todo", type: :feature, js: true do
   end
 
   it "create_user" do
-    result = `bin/todo create_user myname secret -f test.auth`
-    result = `bin/todo users -f test.auth`
+    result = `bin/todo create_user myname secret -c todo.test`
+    result = `bin/todo users -c todo.test`
     expect(result).to eql(
 %Q{1: testuser
 2: myname
@@ -45,7 +38,7 @@ describe "todo", type: :feature, js: true do
     user.lists.create name: 'testlist'
     user.lists.create name: 'testlist2'
 
-    result = `bin/todo lists -f test.auth`
+    result = `bin/todo lists -c todo.test`
     expect(result).to eql(
 %Q{testlist
 testlist2
@@ -55,9 +48,9 @@ testlist2
   it "create_list" do
     user = User.first
     user.lists.create name: 'testlist'
-    result = `bin/todo create_list testlist9 -f test.auth`
+    result = `bin/todo create_list testlist9 -c todo.test`
 
-    result = `bin/todo lists -f test.auth`
+    result = `bin/todo lists -c todo.test`
     expect(result).to eql(
 %Q{testlist
 testlist9
