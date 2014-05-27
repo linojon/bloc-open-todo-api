3673820f3e24e86d00f9a1525719f94f15ed3ab9
Jonathan Linowes <linojon@gmail.com>
2014-05-26 20:45:02 -0400

    couple of todo_specs are working

--- spec/cli/todo_spec.rb
+++ spec/cli/todo_spec.rb
@@ -12,4 +12,14 @@ describe "todo", type: :feature, js: true do
     expect(data['users']).to include( { 'id' => 1, 'username' => 'testuser' } )
   end
 
+  it "login" do
+    `bin/todo login testuser secret 127.0.0.1:3003 -f test.auth`
+    expect(File.read("test.auth").strip).to eql "testuser;secret;127.0.0.1:3003"
+  end
+
+  it "whoami" do
+    result = `bin/todo whoami -f test.auth`
+    expect(result.strip).to eql "1: testuser"
+  end
+
 end
